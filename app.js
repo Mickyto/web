7f7b21fccbf06f339543e8267e30a8bffc7a58fb
Igor Egai <egayigor@gmail.com>
2016-08-11 01:53:55 +0500

    Creating Products and Categories pages

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var categories = require('./routes/categories');
+var products = require('./routes/products');
+
 
 var app = express();
 
@@ -23,7 +25,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/categories', categories);
+app.use('/products', products);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- scripts/script.sh
+++ scripts/script.sh
@@ -9,6 +9,10 @@ echo 'Installing node.js and npm'
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 apt-get install -y nodejs npm
 
+echo 'Launching node.js server on background'
+
+cp /vagrant/scripts/nodeup.conf /etc/init/nodeup.conf
+
 echo 'Installing dependencies'
 
 cd /vagrant
@@ -18,5 +22,4 @@ npm install -g nodemon
 ln /usr/bin/nodejs /usr/bin/node
 
 echo 'Done'
-
 
