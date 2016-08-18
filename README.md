616cbf6080bd2ece1a201766709a6c30d5f13ade
Igor Egai <egayigor@gmail.com>
2016-08-18 13:03:14 +0500

    Add readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# web
+SKUKIT.COM
+==========
+
+[Skukit.com](http://skukit.com) is a global database of industrial products
+
+
+## Installation
+
+```sh
+git clone git@github.com:skukit/web.git
+cd web
+vagrant up
+```
+You can see launched application on http://192.168.33.10
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  http.get('http://api.skukit-dev.com/v1/categories/', function (response) {
+    var buffer = "";
+
+    response.on("data", function (chunk) {
+      buffer += chunk;
+    });
+    console.log(response);
+
+    response.on("end", function (err) {
+      var data = JSON.parse(buffer);
+      res.render('index', {
+        title: 'Express',
+        categories: data.items
+      });
+     });
+  });
 });
 
 module.exports = router;
