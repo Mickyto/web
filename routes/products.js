b8b382b6cc7ee1c69f8c6ad4cb70e79cab9cf741
Igor Egai <egayigor@gmail.com>
2016-09-14 22:00:02 +0300

    Implement product page

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var Client = require('node-rest-client').Client;
 
-router.get('/', function(req, res, next) {
-    res.render('products', { title: 'Products' });
+var client = new Client();
+
+// route middleware to validate :id
+router.param('id', function (req, res, next, id) {
+    client.get('http://api.skukit-st.com/v1/products/' + id, function (data, response) {
+        req.id = id;
+        next();
+    });
 });
+
+router.get('/:id', function (req, res) {
+
+    client.get('http://api.skukit-st.com/v1/products/' + req.id, function (product, response) {
+        client.get('http://api.skukit-st.com/v1/categories/' + product.category_id, function (category, response) {
+
+
+            var imageArray = [];
+            for (var i = 0; i < product.images.length; i++) {
+                var picName = product.images[i].src;
+                if (picName !== undefined) {
+                    imageArray[i] = {
+                        src: 'http://im.skukit-st.com/' + picName[0] + '/' + picName[1] + '/' + picName[2] + '/' + picName
+                    }
+                }
+            }
+
+            console.log(imageArray);
+
+
+
+
+            //var picName = product.front_image.src;
+            //var src = 'http://im.skukit-st.com/' + picName[0] + '/' + picName[1] + '/' + picName[2] + '/' + picName;
+
+            res.render('product', {
+                title: product.name,
+                product: product,
+                images: imageArray,
+                category: category.name
+            });
+        });
+    });
+});
+
 
 module.exports = router;
 
