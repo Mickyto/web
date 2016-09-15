d24d398b511eb4a9677f75a33fbde9ed091bea57
Igor Egai <egayigor@gmail.com>
2016-09-15 11:39:34 +0300

    Create catalog page /catalog/

--- routes/index.js
+++ routes/index.js
@@ -27,4 +27,28 @@ router.get('/', function(req, res) {
     });
 });
 
+router.get('/catalog/', function(req, res) {
+    client.get('http://api.skukit-st.com/v1/categories/', function (categories) {
+        var categoryArray = [];
+        for (var i = 0; i < categories.items.length; i++) {
+            var picName = categories.items[i].ico;
+            categoryArray[i] = {
+                name: categories.items[i].name,
+                id: categories.items[i]._id
+            };
+            if (picName !== undefined) {
+                var src = 'http://im.skukit-st.com/' + picName[0] + '/' + picName[1] + '/' + picName[2] + '/' + picName;
+                categoryArray[i].src = src;
+            }
+        }
+        res.render('catalog', {
+            title: 'catalog',
+            categories: categoryArray
+        });
+    });
+});
+
+
+
+
 module.exports = router;
