5c53bfab2f97129d24bb89b91bd5a5e5bf5a89e5
Igor Egai <egayigor@gmail.com>
2016-09-15 11:29:09 +0300

    Display list of vendors on index page

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var Client = require('node-rest-client').Client;
-
 var client = new Client();
 
 router.get('/', function(req, res) {
-
-    client.get('http://api.skukit-st.com/v1/categories/', function (data, response) {
-        res.render('index', {
-            title: 'skukit HOME',
-            categories: data.items
+    client.get('http://api.skukit-st.com/v1/categories/', function (categories) {
+        client.get('http://api.skukit-st.com/v1/vendors/', function (vendors) {
+            var vendorArray = [];
+            for (var i = 0; i < vendors.items.length; i++) {
+                var picName = vendors.items[i].logotype;
+                vendorArray[i] = {
+                    name: vendors.items[i].name,
+                    id: vendors.items[i]._id
+                };
+                if (picName !== undefined) {
+                    var src = 'http://im.skukit-st.com/' + picName[0] + '/' + picName[1] + '/' + picName[2] + '/' + picName;
+                    vendorArray[i].src = src;
+                }
+            }
+            res.render('index', {
+                title: 'skukit HOME',
+                categories: categories.items,
+                vendors: vendorArray
+            });
         });
     });
 });
