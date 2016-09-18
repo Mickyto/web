b9cb3649af9cd8e4418cf99340e0e43c1752f965
Igor Egai <egayigor@gmail.com>
2016-09-18 20:54:31 +0300

    Add shipitfile.js

---
+++ shipitfile.js
@@ -0,0 +1,28 @@
+module.exports = function (shipit) {
+    require('shipit-deploy')(shipit);
+
+    shipit.initConfig({
+        default: {
+            workspace: '/tmp/git-monitor',
+            deployTo: '/usr/app/web',
+            repositoryUrl: 'https://github.com/Mickyto/web.git',
+            ignores: ['PRD.md', '.gitignore/', 'README.md', 'nightwatch.json', 'nightwatch', 'Vagrantfile'],
+            keepReleases: 2,
+            key: '~/.ssh/id_rsa',
+            branch: 'testing_deploy'
+        },
+        staging: {
+            servers: 'root@95.213.204.155'
+        }
+    });
+
+    shipit.task('build', function () {
+        return shipit.remote('if test out=$(docker images | grep web) ; then cd /usr/app/web/current && docker build -t web .' +
+            '&& docker run --rm -v /usr/app/web/current:/usr/src/app web npm install' +
+            '&& docker run -d -p 80:3000 -v /usr/app/web/current:/usr/src/app --name webApp web; fi');
+    });
+
+    shipit.task('restart', function () {
+        return shipit.remote('if out=$(docker ps | grep webApp) ; then docker restart webApp; fi');
+    });
+};
